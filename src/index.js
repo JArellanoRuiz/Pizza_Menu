29b24cd1f728954a153f718ef675eca64dca9355
JArellanoRuiz <jarare@gmail.com>
2023-09-29 10:02:45 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import './index.css';
+
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+  
+
+
+function App(){
+    return (
+        <div className="container">
+            <Header/>
+            <Menu/>
+            <Footer/>
+        </div>
+    )
+}
+
+
+function Header () {
+    return (
+        <header className="header">
+            <h1>Javier's Pizza Co.</h1>
+        </header>
+    )
+}
+
+function Menu() {
+
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
+
+    return (
+        <main className="menu">
+            <h2>Our Menú</h2>
+            
+        {numPizzas >0 ? (
+            <>
+                <p>
+                Aunthentic Intalian Pizzas!. 6 creative dishes to choose from.
+                All from our stone oven, all organic, all just tasty!
+                </p>
+
+                <ul className='pizzas'>
+                {pizzaData.map(pizza => (
+                    <Pizza pizzaObj={pizza} key={pizza.name}/>
+                    ))} 
+                </ul>
+            </>
+        ) : <p>We're Working on our menu. 😊</p>}
+
+        </main> 
+    )
+}
+
+
+function Pizza({pizzaObj}){
+    //console.log(pizzaObj);
+
+    //if(pizzaObj.soldOut) return null;
+
+    return (
+        <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ""}`}>
+            <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
+            <div>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}</p>
+
+                <span>{pizzaObj.soldOut ? 'SOLD OUT' : `usd $${pizzaObj.price}`}</span>
+            </div>
+        </li>
+    )
+}
+
+
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour=10;
+    const closeHour=22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+
+
+    return (
+        <footer className='Footer'>
+            {isOpen ? (
+               <Order closeHour={closeHour} openHour={openHour}/>
+            ) :(
+            
+            <div className="order">
+                    <p>
+                        We're Close until {openHour}:00. Visit us soon!.
+                    </p>
+                </div>
+
+            )}
+        </footer>
+    )
+
+    // if(isOpen){
+    //     return <footer className="footer">{new Date().toLocaleTimeString()}. Currently Open!</footer>
+    // }else{
+    //     return <footer className="footer">{new Date().toLocaleTimeString()}. Currently Close.  </footer>
+    // }
+}
+
+function Order ({closeHour,openHour}) {
+
+    return(
+    <div className="order">
+        <p>
+            We're open from {openHour}:00 until {closeHour}:00. Visit us or Order Online!.
+        </p>
+        <button className='btn'>Order Now.</button>
+    </div>
+    )    
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
+
+
+
+
